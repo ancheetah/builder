49450f3f5c112be27d35b7ddb0a07cc03f491381
AJ <ancheetah@users.noreply.github.com>
2022-01-10 18:20:09 -0500

    heading title

--- examples/next-js-simple/components/Heading.tsx
+++ examples/next-js-simple/components/Heading.tsx
@@ -1,14 +1,15 @@
-import { Builder } from "@builder.io/react";
+import { Builder, BuilderContent } from "@builder.io/react";
 import React from 'react';
 
 export const Heading = (props: { image: string; title: string; }) => {
 console.log(props.image);
-return(
-<div style={{'width': '50vw'}}>
-  <h1>{props.image}</h1>
-  <img src={props.image} width='100'></img>
-</div>
-)
+  return(
+    <div style={{'width': '50vw'}}>
+      <h1>{props.title}</h1>
+      <p>{props.image}</p>
+      <img src={props.image} width='100'></img>
+    </div>
+  )
 }
 
 Builder.registerComponent(Heading, {
