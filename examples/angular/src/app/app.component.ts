0d27047b6b8d5dd4784548f430f3cdf90a0d080f
AJ <ancheetah@users.noreply.github.com>
2022-03-16 16:59:56 -0400

    html api bug reproduction first pass

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { BuilderBlock } from '@builder.io/angular';
 import { Component, Input } from '@angular/core';
 import { GetContentOptions } from '@builder.io/sdk';
+import { HttpClient } from '@angular/common/http';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'custom-thing',
@@ -36,10 +38,25 @@ export class AppComponent {
     }
   };
 
+  constructor(httpClient: HttpClient) {};
+
   data = {
     property: 'hello',
     fn: (text: string) => alert(text),
   };
+
+  ngOnInit() {
+
+    // fetch from HTML API
+    this.httpClient
+      .get('https://cdn.builder.io/api/v1/html/page?apiKey=c8c2ea5f476a464ab3f9f1f3f9c83a2a&url=/en/html-api-test-component')
+      .pipe(map<any, any>((response: any) => response));
+    
+    this.page = response.data.html
+
+    // in a test builder space, create a page and use the Custom Thing component
+    // if Custom Thing renders then this is on them
+  }
 
   load(event: any) {
     console.log('load', event);
