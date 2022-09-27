fc03fad7f8b7b514041c49a2c61a1fc2f9ab6f52
AJ <ancheetah@users.noreply.github.com>
2022-09-26 20:29:48 -0400

    functional component, react-lite

---
+++ examples/next-js-simple/components/Image.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Image (props: any) {
+    return <img src={props.image} width="100" height="100"/>
+} 
+
+export default Image;
--- examples/next-js-simple/pages/[[...page]].tsx
+++ examples/next-js-simple/pages/[[...page]].tsx
@@ -1,23 +1,43 @@
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import { useRouter } from 'next/router'
-import { BuilderComponent, builder, Builder, useIsPreviewing } from '@builder.io/react'
+import { BuilderComponent, builder, Builder, useIsPreviewing } from '@builder.io/react/lite'
 import DefaultErrorPage from 'next/error'
 import Head from 'next/head'
 import builderConfig from '@config/builder'
+// import dynamic from 'next/dynamic'
 // loading widgets dynamically to reduce bundle size, will only be included in bundle when is used in the content
 import '@builder.io/widgets/dist/lib/builder-widgets-async'
 
-import "../components/Heading";
+import "../components/Image"
+import "../components/Heading"
 
 // const BUILDER_API_KEY = '79c606108cdf4936815f4736565ac6ee'
 builder.init(builderConfig.apiKey)
+Builder.registerComponent(
+  // dynamic(() => import('../components/Image')), 
+  Image,
+  {
+    name: 'Image',
+    override: true,
+    inputs: [
+      { 
+        name: 'image', 
+        type: 'file',      
+        defaultValue: 'https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F1ab2f86b7c5447ad9b99cb039165c15e?width=799',
+      },
+    ],
+})
 
 Builder.register('insertMenu', {
   name: 'My Components',
   items: [
     { name: 'Heading' },
+    { name: 'Image'}
   ],
 })
+
+// MyImage.jsx
+
 
 const locale = 'en-US'
 
